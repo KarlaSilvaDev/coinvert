00fe1296ad1b8f74e7369f39c014e2cf75f980ee
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-04-23 17:05:21 -0300

    modified: class Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import models.Converter;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Converter converter = new Converter();
+        int option = 0;
+
+        while(option < 1 || option > 7) {
+            converter.showMenu();
+            option = scanner.nextInt();
+        }
+
+        System.out.println("Digite o valor a ser convertido: ");
+        double value = scanner.nextDouble();
+
+        System.out.println(converter.convertCurrency(value, option));
+    }
 }
